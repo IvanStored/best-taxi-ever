7f50aff1f12a0c77d52a3c127efe5f5308e5e643
Ivan Antonishak <jus1stored@gmail.com>
2022-11-10 15:04:19 +0200

    Add templates for drivers, fix urls.py

--- best_taxi_ever/urls.py
+++ best_taxi_ever/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("taxi.urls", namespace="taxi"))
+    path("", include("taxi.urls", namespace="taxi")),
+    path("ratings/", include('star_ratings.urls', namespace="ratings"))
 ]
