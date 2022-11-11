d6740e9ac0a10f9ed9885d5aa46c66fe5eeabb4d
Ivan Antonishak <jus1stored@gmail.com>
2022-11-11 17:20:28 +0200

    Create models for threads and messages

--- social/models.py
+++ social/models.py
@@ -1,3 +1,31 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class ThreadModel(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="+"
+    )
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="+"
+    )
+    has_unread = models.BooleanField(default=False)
+
+
+class MessageModel(models.Model):
+    thread = models.ForeignKey(
+        "ThreadModel",
+        related_name="+",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    sender_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="+"
+    )
+    receiver_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="+"
+    )
+    body = models.CharField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
